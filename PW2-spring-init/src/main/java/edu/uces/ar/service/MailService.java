06487c1b49e408a0db9564512fa911e0658f2c67
german_pesce <germanpesce@gmail.com>
2020-04-26 01:25:01 -0300

    Integración de servicio para envío de emails

---
+++ PW2-spring-init/src/main/java/edu/uces/ar/service/MailService.java
@@ -0,0 +1,26 @@
+package edu.uces.ar.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+	  
+    private JavaMailSender emailSender;
+ 
+    public MailService(JavaMailSender emailSender) {
+		super();
+		this.emailSender = emailSender;
+	}
+
+	public void sendSimpleMessage(String to, String subject, String text) {
+
+    	SimpleMailMessage message = new SimpleMailMessage(); 
+        message.setTo(to); 
+        message.setSubject(subject); 
+        message.setText(text);
+        emailSender.send(message);
+        
+    }
+}
--- PW2-spring-init/src/test/java/edu/uces/ar/springinit/Pw2SpringInitApplicationTests.java
+++ PW2-spring-init/src/test/java/edu/uces/ar/springinit/Pw2SpringInitApplicationTests.java
@@ -1,13 +1,22 @@
 package edu.uces.ar.springinit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import edu.uces.ar.service.MailService;
 
 @SpringBootTest
 class Pw2SpringInitApplicationTests {
-
+	
+	@Autowired
+	MailService mailService;
+	
 	@Test
-	void contextLoads() {
+	void emailSend() {
+		
+		mailService.sendSimpleMessage("profesor.german.pesce@gmail.com", "Enviando Mail desde Spring", "Probando probando!");
+		
 	}
 
 }
