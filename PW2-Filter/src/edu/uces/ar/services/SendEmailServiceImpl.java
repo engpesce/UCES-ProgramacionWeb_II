b5d0e5166b1f6dc18ff26ee043b390554b241c1e
german_pesce <germanpesce@gmail.com>
2020-09-04 19:40:32 -0300

    Cambios para envio de mail mediante PaperCut

--- PW2-Filter/src/edu/uces/ar/services/SendEmailServiceImpl.java
+++ PW2-Filter/src/edu/uces/ar/services/SendEmailServiceImpl.java
@@ -1,9 +1,9 @@
 package edu.uces.ar.services;
 
-import java.util.Date;
 import java.util.Properties;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
@@ -11,47 +11,37 @@ import javax.mail.internet.MimeMessage;
 
 public class SendEmailServiceImpl implements SendEmailService{
 
-	private final static String SMTP_ACCOUNT_SERVER = "smtp.gmail.com";
-	private final static String SMTP_ACCOUNT_NAME = "usuario@gmail.com";
-	private final static String SMTP_ACCOUNT_PASS = "password";
+	private final static String SMTP_SERVER_HOST = "localhost";
+	private final static String SMTP_SERVER_PORT = "25";
 	
 	@Override
-	public boolean sendEmail(String from, String to, String subject, String message) {
+	public boolean sendEmail(String from, String to, String subject, String text) {
 		
-		Properties props = new Properties();
-		props.put("mail.smtp.host", "smtp.gmail.com");
-		props.put("mail.smtp.port", "587");
-		props.put("mail.debug", "true");
-		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.localhost", "FilterAndEmailExample");
-		  
-		Session s = Session.getInstance(props, null);
-		s.setDebug(true);
-		  
-		MimeMessage mimeMessage = new MimeMessage(s);
-		
-		try {
-			InternetAddress fromAddress = new InternetAddress(from, from);
-			InternetAddress toAddress = new InternetAddress(to);
-			  
-			mimeMessage.setSentDate(new Date());
-			mimeMessage.setFrom(fromAddress);
-			mimeMessage.addRecipient(Message.RecipientType.TO, toAddress);
-			  
-			mimeMessage.setSubject(subject);
-			mimeMessage.setContent(message, "text/plain");
-			  
-			Transport tr = s.getTransport("smtp");
-			tr.connect(SMTP_ACCOUNT_SERVER, SMTP_ACCOUNT_NAME, SMTP_ACCOUNT_PASS);
-			mimeMessage.saveChanges();
-			tr.sendMessage(mimeMessage, mimeMessage.getAllRecipients());
-			tr.close();
-			
-		} catch (Exception e) {
-			System.err.println("Error: " + e.getMessage() + ". Causa: " + e.getCause());
-		}
-		return false;
+	      Properties properties = System.getProperties();
+	      properties.setProperty("mail.smtp.host", SMTP_SERVER_HOST);
+	      properties.setProperty("mail.smtp.port", SMTP_SERVER_PORT);
+
+	      Session session = Session.getDefaultInstance(properties);
+	      try {
+	         // Create a default MimeMessage object.
+	         MimeMessage message = new MimeMessage(session);
+	         // Set From: header field of the header.
+	         message.setFrom(new InternetAddress(from));
+	         // Set To: header field of the header.
+	         message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+	         // Set Subject: header field
+	         message.setSubject(subject);
+	         // Now set the actual message
+	         message.setText(text);
+	         // Send message
+	         Transport.send(message);
+	         System.out.println("Message sent successfully....");
+	      }catch (MessagingException mex) {
+	         mex.printStackTrace();
+	         return false;
+	      }
+
+		return true;
 	}
 
 	
